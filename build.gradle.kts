4fc91de54c60b7100276c6fd5f6e3bd8a23fa88e
amannocci <adrien.mannocci@gmail.com>
2023-06-04 14:51:10 +0200

    feat: support event payload mutate
    Signed-off-by: amannocci <adrien.mannocci@gmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,6 +20,7 @@ val guavaVersion = "31.1-jre"
 val jcToolsVersion = "4.0.1"
 val configVersion = "1.4.2"
 val picocliVersion = "4.7.3"
+val commonsTextVersion = "1.10.0"
 
 val launcherClassName = "io.techcode.fluxy.Fluxy"
 
@@ -34,6 +35,7 @@ dependencies {
   implementation("com.typesafe:config:$configVersion")
   implementation("org.jctools:jctools-core:$jcToolsVersion")
   implementation("info.picocli:picocli:$picocliVersion")
+  implementation("org.apache.commons:commons-text:$commonsTextVersion")
   testImplementation("io.vertx:vertx-junit5")
   testImplementation("org.junit.jupiter:junit-jupiter:$junitJupiterVersion")
 }
--- src/main/java/io/techcode/fluxy/module/core/MutateFlow.java
+++ src/main/java/io/techcode/fluxy/module/core/MutateFlow.java
@@ -1,14 +1,30 @@
 package io.techcode.fluxy.module.core;
 
+import com.google.common.collect.Lists;
 import com.typesafe.config.Config;
 import io.techcode.fluxy.component.Flow;
 import io.techcode.fluxy.event.Event;
 import io.techcode.fluxy.pipeline.Pipeline;
+import io.vertx.core.json.pointer.JsonPointer;
+import org.apache.commons.text.StringSubstitutor;
+import org.apache.commons.text.lookup.StringLookupFactory;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
 
 public class MutateFlow extends Flow {
 
+  private final List<Consumer<Event>> tasks;
+
   public MutateFlow(Pipeline pipeline, Config options) {
     super(pipeline, options);
+    tasks = Lists.newArrayList();
+    var tasksConf = options.getConfig("tasks");
+    if (tasksConf.hasPath("set")) {
+      tasks.add(new SetTask(tasksConf.getConfig("set")));
+    }
   }
 
   @Override
@@ -18,8 +34,47 @@ public class MutateFlow extends Flow {
 
   @Override
   protected void onPush(Event evt) {
-    evt.payload().put("mutate", "test");
+    for (var task : tasks) {
+      task.accept(evt);
+    }
     out.pushOne(evt);
+  }
+
+  private static class SetTask implements Consumer<Event> {
+
+    private final StringSubstitutor interpolator;
+    private final JsonPointer targetPointer;
+    private final String targetValue;
+    private Event currentEvt;
+
+    public SetTask(Config options) {
+      var lookups = StringLookupFactory.INSTANCE.interpolatorStringLookup(new HashMap<>() {{
+        put(StringLookupFactory.KEY_ENV, StringLookupFactory.INSTANCE.environmentVariableStringLookup());
+        put(StringLookupFactory.KEY_JAVA, StringLookupFactory.INSTANCE.javaPlatformStringLookup());
+        put(StringLookupFactory.KEY_SYS, StringLookupFactory.INSTANCE.systemPropertyStringLookup());
+        put("eventPayload", StringLookupFactory.INSTANCE.functionStringLookup(new Function<String, String>() {
+          @Override
+          public String apply(String key) {
+            var pointer = JsonPointer.from(key);
+            return String.valueOf(pointer.queryJson(currentEvt.payload()));
+          }
+        }));
+      }}, StringLookupFactory.INSTANCE.nullStringLookup(), false);
+      interpolator = new StringSubstitutor(lookups).setEnableSubstitutionInVariables(true);
+      targetPointer = JsonPointer.from(options.getString("target"));
+      targetValue = options.getString("value");
+    }
+
+    @Override
+    public void accept(Event evt) {
+      try {
+        currentEvt = evt;
+        var value = interpolator.replace(targetValue);
+        targetPointer.writeJson(evt.payload(), value, true);
+      } finally {
+        currentEvt = null;
+      }
+    }
   }
 
 }
